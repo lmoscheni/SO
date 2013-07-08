03bd07c156fcc239a9646e052b3e08e796a1fa67
lmoscheni <leandromoscheni@gmail.com>
2013-07-07 21:53:04 -0300

    Actualizo el Kernel y el PCB

--- PCB.py
+++ PCB.py
@@ -11,12 +11,13 @@ class PCB():
     '''
 
 
-    def __init__(self,pid,pc,fin):
+    def __init__(self,pid,pc,fin,priority):
         self.PID = pid
         self.pc = pc
         self.inicio = pc
         self.fin = fin
         self.state = "Ready"
+        self.priority = priority
         
     def getPID(self):
         return self.PID
@@ -36,4 +37,5 @@ class PCB():
     def setState(self,value):
         self.state = value
         
-        
+    def getPriority(self):
+        return self.priority
